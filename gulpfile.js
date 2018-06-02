f0d0c13252c440c7ec5f9dd3d27d8acb3068e117
yokoee <yokoee@live.com>
2018-06-02 22:58:31 +0800

    v3.0 released

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,39 @@
 const gulp = require('gulp')
-const less = require('gulp-less')
-const connect = require('gulp-connect')
+const babel = require('gulp-babel')
+const minify = require('gulp-minify')
 const autoprefixer = require('gulp-autoprefixer')
+const cleanCSS = require('gulp-clean-css')
+const rename = require('gulp-rename')
 
-gulp.task('less', () => {
-    gulp.src('src/*.less')
-        .pipe(less())
+gulp.task('css', () => {
+    gulp.src('src/*.css')
         .pipe(autoprefixer({
             browsers: ['defaults'],
+        }))
+        .pipe(cleanCSS())
+        .pipe(rename({
+            suffix: '.min'
         }))
         .pipe(gulp.dest('dist'))
 })
 
-gulp.task('connect', () => {
-    connect.server({
-        livereload: true,
-        port: 8080,
-    });
+gulp.task('js', () => {
+    gulp.src('src/*.js')
+        .pipe(babel({
+            presets: ['env']
+        }))
+        .pipe(minify({
+            noSource: true,
+            ext: {
+                min: '.min.js'
+            }
+        }))
+        .pipe(gulp.dest('dist'))
 })
 
-gulp.task('watch', ['connect'], () => {
-    gulp.watch('src/*.less', ['less'])
+gulp.task('copyLib', () => {
+    gulp.src('src/lib')
+        .dest('dist/lib')
 })
 
-gulp.task('default', ['watch'])
+gulp.task('default', ['css', 'js', 'copyLib'])
